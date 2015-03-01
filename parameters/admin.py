c472c6c349856dfd227b71266c4142fd31d690de
Mark Mikofski <bwana.marko@yahoo.com>
2015-03-01 12:31:45 -0800

    make admin page more readable

--- parameters/admin.py
+++ parameters/admin.py
@@ -4,19 +4,23 @@ from parameters.models import PVInverter
 
 class PVInverterAdmin(admin.ModelAdmin):
     list_display = (
-        'Sandia_ID', 'source', 'manufacturer', 'name', 'vintage',
+        'manufacturer', 'name', 'vintage', 'source', 'Sandia_ID',
         'Vaco', 'Paco', 'Vdco', 'Pdco', 'Pso', 'C0', 'C1', 'C2',
         'C3', 'Vdcmax', 'Idcmax', 'MPPT_low', 'MPPT_hi', 'Pnt',
         'Tamb_low', 'Tamb_max', 'weight', 'numberMPPTChannels'
     )
-    fields = (('manufacturer', 'name', 'vintage'),
+    fields = ('manufacturer',
+              ('name', 'vintage'),
               ('Sandia_ID', 'source'),
-              ('Vaco', 'Paco'), ('Vdco', 'Pdco'), 
-              ('Pso', 'C0', 'C1', 'C2', 'C3'),
+              ('Vaco', 'Paco'),
+              ('Vdco', 'Pdco'), 
+              ('C0', 'C1'),
+              ('C2', 'C3'),
+              ('Pso', 'Pnt'),
               ('Vdcmax', 'Idcmax'),
               ('MPPT_low', 'MPPT_hi'),
-              ('Pnt', 'Tamb_low', 'Tamb_max', 'weight'),
-              'numberMPPTChannels')
+              ('Tamb_low', 'Tamb_max'),
+              ('weight', 'numberMPPTChannels'))
 
 # Register your models here.
 admin.site.register(PVInverter, PVInverterAdmin)
