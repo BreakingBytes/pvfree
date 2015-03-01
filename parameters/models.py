29a053cc389b0b589257c56eb3c56cdcba5af0d4
Mark Mikofski <bwana.marko@yahoo.com>
2015-02-28 23:19:43 -0800

    fixes to upload
    * need integrity error, not validation error when saving models to db
    * make a default date = 1999
    * add docstring and comments
    * only append headers listed in mapping
    * compare number of headers to number of keys in mapping
    * loop over list of should be floats, add weight to list
    * use get(key,dflt) for Vaco and vintage in update
    * need vintage key in get()
    * add output "already exists"

--- parameters/models.py
+++ parameters/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, IntegrityError
 from django.core.exceptions import ValidationError
 from openpyxl import load_workbook
 from datetime import date
@@ -39,7 +39,7 @@ class PVInverter(models.Model):
     manufacturer = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     source = models.CharField(max_length=10)
-    vintage = models.DateField()
+    vintage = models.DateField(default=date(1999, 1, 1))
     Paco = models.FloatField(default=-999)
     Vaco = models.FloatField(default=-999)
     Pdco = models.FloatField(default=-999)
@@ -73,48 +73,74 @@ class PVInverter(models.Model):
 
     @classmethod
     def upload(cls, filename, sheet=None, mapping=MAPPING):
+        """
+        Class method for creating and updating records from file.
+
+        :param filename: Name of file.
+        :param sheet: Name of sheet (None).
+        :param mapping: Map of worksheet headers to model fields.
+        :type mapping: dict
+        """
+        # load workbook
         wb = load_workbook(filename, use_iterators = True)
+        # use first sheet by default
         if not sheet:
             sheet = wb.get_sheet_names()[0]
+        # load worksheet
         ws = wb.get_sheet_by_name(sheet)
-        rows = ws.iter_rows()
-        headers = [row.value for row in rows.next()]
-        if not all(headers):
-            raise ValidationError('There is a missing header.')
-        if not all(isinstance(h, basestring) for h in headers):
-            raise ValidationError('All headers must be strings.')
+        rows = ws.iter_rows()  # row iterator
+        # get headers
+        headers = [row.value for row in rows.next() if row.value in mapping]
+        # check headers match mapping
+        if len(headers) < len(mapping):
+            raise ValidationError('There are missing or incorrect headers.')
+        # iterate over all rows
         for row in rows:
-            kwargs = dict([(mapping[h], r.value) for h, r in zip(headers, row) if r.value])
-            if 'vintage' in kwargs: kwargs['vintage'] = date(kwargs['vintage'], 1, 1)
-            if 'Tamb_low' in kwargs:
-                try:
-                    Tamb_low = float(kwargs['Tamb_low'])
-                except ValueError:
-                    kwargs.pop('Tamb_low')
-            if 'Tamb_hi' in kwargs:
-                try:
-                    Tamb_hi = float(kwargs['Tamb_hi'])
-                except ValueError:
-                    kwargs.pop('Tamb_hi')
+            # using Python-2.6 - doesn't have dictionary comprehension
+            # zip will limit size to smaller list, ie: headers
+            kwargs = dict([(mapping[h], r.value) for h, r in zip(headers, row)
+                           if r.value])
+            # convert vintage to date
+            if 'vintage' in kwargs:
+                kwargs['vintage'] = date(kwargs['vintage'], 1, 1)
+            # ducktype temperature range for floats, pop otherwise
+            should_be_floats = ('Tamb_low', 'Tamb_max', 'weight', 'Pnt')
+            for val in should_be_floats:
+                if val in kwargs:
+                    try:
+                        float(kwargs[val])
+                    except ValueError:
+                        kwargs.pop(val)
+                        # TODO: put these in a "notes" catchall field
             try:
+                # create new PVInverter record
                 pvinv, created = cls.objects.get_or_create(**kwargs)
-            except ValidationError as err:
+            except (IntegrityError, ValidationError) as err:
+                # get key of existing record
+                vintage = kwargs.get('vintage', date(1999, 1, 1))
+                Vaco = kwargs.get('Vaco', -999)
                 if cls.objects.filter(manufacturer=kwargs['manufacturer'],
-                                      name=kwargs['name'],
-                                      Vaco=kwargs['Vaco'],
-                                      vintage=kwargs['vintage']).exists():
+                                      name=kwargs['name'], Vaco=Vaco,
+                                      vintage=vintage).exists():
+                    if Vaco != -999:
+                        kwargs.pop('Vaco')
+                    if vintage != date(1999, 1, 1):
+                        kwargs.pop('vintage')
                     pvinv = cls.objects.get(
                         manufacturer=kwargs.pop('manufacturer'),
-                        name=kwargs.pop('name'),Vaco=pop('Vaco'),
-                        vintage=kwargs.pop('vintage'))
+                        name=kwargs.pop('name'),Vaco=Vaco, vintage=vintage)
+                    # update existing record
                     for k, v in kwargs.iteritems():
                         setattr(pvinv, k, v)
                     pvinv.save()
                     created = True
                 else:
-                    raise err
+                    raise err  # raise caught exception
             if created:
+                # TODO: use logger
                 print 'created or updated %s' % pvinv
+            else:
+                print '%s already exists' % pvinv
             created = False
 
 
