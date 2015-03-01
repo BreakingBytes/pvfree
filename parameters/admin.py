6d64fd050b234bb91f317cb33d1d7daa1d44e5ba
Mark Mikofski <bwana.marko@yahoo.com>
2015-03-01 14:06:10 -0800

    Paco filter

--- parameters/admin.py
+++ parameters/admin.py
@@ -5,7 +5,7 @@ from parameters.models import PVInverter
 class VacoRangeFilter(admin.SimpleListFilter):
     # Human-readable title which will be displayed in the
     # right admin sidebar just above the filter options.
-    title = ('Vaco range')
+    title = 'Vaco'
 
     # Parameter for the filter that will be used in the URL query.
     parameter_name = 'Vaco'
@@ -18,19 +18,12 @@ class VacoRangeFilter(admin.SimpleListFilter):
         human-readable name for the option that will appear
         in the right sidebar.
         """
-        return (
-            (-999, ('Missing')),
-            (100, ('1 - 100')),
-            (200, ('101 - 200')),
-            (300, ('201 - 300')),
-            (400, ('301 - 400')),
-            (500, ('401 - 500')),
-            (600, ('501 - 600')),
-            (700, ('601 - 700')),
-            (800, ('701 - 800')),
-            (900, ('801 - 900')),
-            (1000, ('901 - 1000')),
-        )
+        _incr = 100
+        _stop = 1000
+        return tuple(
+            [('-999', 'Missing')] +
+            [(str(x), '%d - %d' % ((x - _incr + 1), x))
+             for x in xrange(_incr, _stop + _incr, _incr)])
 
     def queryset(self, request, queryset):
         """
@@ -49,6 +42,46 @@ class VacoRangeFilter(admin.SimpleListFilter):
                                    Vaco__lte=float(self.value()))
 
 
+class PacoRangeFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = 'Paco'
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'Paco'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        _incr = 100
+        _stop = 1000
+        return tuple(
+            [('-999', 'Missing')] +
+            [(str(x), '%d - %d' % ((x - _incr + 1), x))
+             for x in xrange(_incr, _stop + _incr, _incr)])
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        # Compare the requested value (either '80s' or '90s')
+        # to decide how to filter the queryset.
+        if not self.value():
+            return
+        elif self.value() == '-999':
+            return queryset.filter(Paco=float(self.value()))                
+        else:
+            return queryset.filter(Paco__gt=(float(self.value()) - 100),
+                                   Paco__lte=float(self.value()))
+
+
 class PVInverterAdmin(admin.ModelAdmin):
     list_display = (
         'full_name', 'source', 'Sandia_ID', 'manufacturer', 'name', 'vintage',
@@ -56,7 +89,7 @@ class PVInverterAdmin(admin.ModelAdmin):
         'Vdcmax', 'Idcmax', 'MPPT_low', 'MPPT_hi', 'Pnt', 'Tamb_low',
         'Tamb_max', 'weight', 'numberMPPTChannels'
     )
-    list_filter = ('manufacturer', 'vintage', VacoRangeFilter)
+    list_filter = (VacoRangeFilter, PacoRangeFilter, 'vintage', 'manufacturer')
     fields = (('manufacturer', 'name'), 'vintage',
               ('Sandia_ID', 'source'),
               ('Vaco', 'Paco'),
