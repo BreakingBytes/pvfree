42d87757a7a2ccf711e1bbc0dd7611b3fd20200c
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2015-03-01 20:34:15 +0100

    add list and fields to pvinverter admin

--- parameters/admin.py
+++ parameters/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from parameters.models import PVInverter
 
+
+class PVInverterAdmin(admin.ModelAdmin):
+    list_display = (
+        'Sandia_ID', 'source', 'manufacturer', 'name', 'vintage',
+        'Vaco', 'Paco', 'Vdco', 'Pdco', 'Pso', 'C0', 'C1', 'C2',
+        'C3', 'Vdcmax', 'Idcmax', 'MPPT_low', 'MPPT_hi', 'Pnt',
+        'Tamb_low', 'Tamb_max', 'weight', 'numberMPPTChannels'
+    )
+    fields = (('manufacturer', 'name', 'vintage'),
+              ('Sandia_ID', 'source'),
+              ('Vaco', 'Paco'), ('Vdco', 'Pdco'), 
+              ('Pso', 'C0', 'C1', 'C2', 'C3'),
+              ('Vdcmax', 'Idcmax'),
+              ('MPPT_low', 'MPPT_hi'),
+              ('Pnt', 'Tamb_low', 'Tamb_max', 'weight'),
+              'numberMPPTChannels')
+
 # Register your models here.
-admin.site.register(PVInverter)
+admin.site.register(PVInverter, PVInverterAdmin)
 
