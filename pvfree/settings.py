34d93f0c572272803f564d12a0448eff53fdf8e3
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2017-03-29 01:17:46 -0700

    define DATABASES

--- pvfree/settings.py
+++ pvfree/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'pvfree.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
