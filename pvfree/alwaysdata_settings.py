791e356236d561e218182e5eebd82e6f5b8cf181
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2017-03-29 01:15:06 -0700

    separate alwaysdata settings

---
+++ pvfree/alwaysdata_settings.py
@@ -0,0 +1,137 @@
+"""
+Django settings for pvfree project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+import os
+from pvfree import get_secret
+import socket
+
+HOSTNAME = socket.gethostname()  # hostname of machine
+FQDN = socket.getfqdn(HOSTNAME)  # fully qualified domain name of host
+IP_ADDR = socket.gethostbyname(HOSTNAME)  # IP address of host
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+_SECRETKEY = get_secret('SECRET_KEY', '.secretkey')
+SECRET_KEY = _SECRETKEY
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+#ALLOWED_HOSTS = ['pvfree.alwaysdata.net']
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'parameters',
+    'tastypie',
+    'south',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'pvfree.urls'
+
+WSGI_APPLICATION = 'pvfree.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+PSQL_PSWD = get_secret('PSQL_PSWD', '.postgre')
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pvfree_db',
+        'USER': 'pvfree',
+        'PASSWORD': PSQL_PSWD,
+        'HOST': 'postgresql.alwaysdata.com',
+        'PORT': 5432,
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public', 'media')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('[%(levelname)s] %(asctime)s (%(processName)s) ' +
+                       '(%(threadName)s)\n> %(message)s')
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'pvfree.log'),
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'parameters.models': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
