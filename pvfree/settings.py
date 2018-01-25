1dc9b0ee24260f6c34fd2d281bd27bf44b6b1804
Mark Mikofski <bwana.marko@yahoo.com>
2018-01-25 01:35:16 -0800

    don't need socket import
    * just specify allowed host explicitly now
    * use get_secret() in dev-settings

--- pvfree/settings.py
+++ pvfree/settings.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 import os
 from pvfree import get_secret
-import socket
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
