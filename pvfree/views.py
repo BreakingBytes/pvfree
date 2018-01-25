9e541cd521faa3a12c44b260e3b7107d042d93fa
Mark Mikofski <bwana.marko@yahoo.com>
2018-01-25 00:05:05 -0800

    use pvlib, mpld3 to plot module efficiency
    * add module efficiency calculation to views.pvmodule_detail
    * use mpld3 to render plot as d3 js and html, add to context
    * change lead text on detail page to say, efficiency versus irradiance
     and temperature
    * put html in div container, align center, use safe filter
    * jinja2 is required mpld3

--- pvfree/views.py
+++ pvfree/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from parameters.models import PVInverter, PVModule
+import matplotlib.pyplot as plt, mpld3
+from pvlib.pvsystem import sapm
+import numpy as np
 
 
 def home(request):
@@ -22,8 +25,22 @@ def pvmodules(request):
 
 def pvmodule_detail(request, pvmodule_id):
     pvmod = get_object_or_404(PVModule, pk=pvmodule_id)
+    fieldnames = PVModule._meta.get_all_field_names()
+    pvmod_dict = {k: getattr(pvmod, k) for k in fieldnames}
+    celltemps = np.linspace(25, 125, 5)
+    effirrad, celltemp = np.meshgrid(np.linspace(0.1, 1, 10), celltemps)
+    results = sapm(effirrad, celltemp, pvmod_dict)
+    eff = results['p_mp'] / effirrad / pvmod.Area * 100
+    fig = plt.figure(figsize=(8, 6))
+    plt.plot(effirrad.T, eff.T)
+    plt.grid()
+    plt.xlabel('effective irradiance, Ee [suns]')
+    plt.ylabel('efficiency [%]')
+    plt.title(pvmod.Name)
+    plt.legend(celltemps)
+    plotdata = mpld3.fig_to_html(fig)
     return render(request, 'pvmodule_detail.html',
-                  {'path': request.path, 'pvmod': pvmod})
+                  {'path': request.path, 'pvmod': pvmod, 'plotdata': plotdata})
 
 
 def cec_modules(request):
