214b522eb3b7aa9d536fa992706b6a3b5fd152c6
Mark Mikofski <bwana.marko@yahoo.com>
2018-12-01 15:15:32 -0800

    use forms for solarposition API
    * add pvfree/forms.py with fields types and validators
    * instead of parsing request in api, just use form and get cleaned data
    * make freq and tz not required, and set defaults in api if None or
     blank, fixes #10
    * return form errors in JsonResponse with status 400, fixes #9
    * add some more checks in api, for defaults, and wrong date order,
     or if pandas alias offset not recognized, and drop timezones
    * can't use ISO8601 with T in dates in django forms, also don't need
     leading zeros or seconds if zeros
    * wrap long lines, fix indents
    * add tests for errors and defaults
    * use form in pvfree.views.pvlib to render form widget in template
    * add form widget to template in new div container, WIP addresses #11
    * also fix indents in pvfree/views.py

---
+++ pvfree/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class SolarPositionForm(forms.Form):
+    lat = forms.FloatField(
+        label='Latitude',
+        validators=[MaxValueValidator(90), MinValueValidator(-90)])
+    lon = forms.FloatField(
+        label='Lonitude',
+        validators=[MaxValueValidator(180), MinValueValidator(-180)])
+    start = forms.DateTimeField(label='Start date/time')
+    end = forms.DateTimeField(label="End date/time")
+    tz = forms.IntegerField(
+        label='timezone', required=False,
+        validators=[MaxValueValidator(12), MinValueValidator(-12)])
+    freq = forms.CharField(label='frequency', max_length=5, required=False)
--- pvfree/tests.py
+++ pvfree/tests.py
@@ -11,8 +11,8 @@ class SolPosTestCase(TestCase):
             'lon': -122.1,
             'freq': 'T',
             'tz': -8,
-            'start': '2018-01-01T07:00:00',
-            'end': '2018-01-01T08:00:00'
+            'start': '2018-01-01 07:00',
+            'end': '2018-01-01 08:00'
         }
         r = self.client.get('/api/v1/pvlib/solarposition/', data)
         self.assertEqual(r.status_code, 200)
@@ -21,8 +21,61 @@ class SolPosTestCase(TestCase):
         times = pd.DatetimeIndex(
             start=data['start'], end=data['end'],
             freq=data['freq'], tz='Etc/GMT{:+d}'.format(-data['tz']))
-        solpos = solarposition.get_solarposition(times, data['lat'], data['lon'])
-        assert np.allclose(times.values.astype(int),
-                           t.values.astype(int))
+        solpos = solarposition.get_solarposition(
+            times, data['lat'], data['lon'])
+        assert np.allclose(
+            times.values.astype(int), t.values.astype(int))
         assert np.allclose(solpos.apparent_zenith, s.apparent_zenith)
         assert np.allclose(solpos.azimuth, s.azimuth)
+
+    def test_solpos_errors(self):
+        data={
+            'lat': 38.2,
+            'lon': -122.1,
+            'freq': 'T',
+            'tz': -8,
+            'start': '2018-1-1 7:00',
+            'end': '2018-1-1 8:00'
+        }
+        r = self.client.get('/api/v1/pvlib/solarposition/', data={})
+        self.assertEqual(r.status_code, 400)
+        errors = r.json()
+        for d in data.keys():
+            if d in ('freq', 'tz'):
+                continue
+            self.assertIn(d, errors)
+            self.assertEqual('This field is required.', errors[d][0])
+        # check latitude max value
+        data['lat'] = 99.9
+        r = self.client.get('/api/v1/pvlib/solarposition/', data)
+        self.assertEqual(r.status_code, 400)
+        errors = r.json()
+        self.assertIn('lat', errors)
+        self.assertEqual(
+            'Ensure this value is less than or equal to 90.', errors['lat'][0])
+        # check latitude min value
+        data['lat'] = -99.9
+        r = self.client.get('/api/v1/pvlib/solarposition/', data)
+        self.assertEqual(r.status_code, 400)
+        errors = r.json()
+        self.assertIn('lat', errors)
+        self.assertEqual(
+            'Ensure this value is greater than or equal to -90.',
+            errors['lat'][0])
+
+    def test_solpos_defaults(self):
+        data={
+            'lat': 38.2,
+            'lon': -122.1,
+            'freq': 'T',
+            'tz': -8,
+            'start': '2018-1-1 7:00',
+            'end': '2018-1-1 8:00'
+        }
+        freq = data.pop('freq')
+        r = self.client.get('/api/v1/pvlib/solarposition/', data=data)
+        self.assertEqual(r.status_code, 200)
+        data['freq'] = freq
+        tz = data.pop('tz')
+        r = self.client.get('/api/v1/pvlib/solarposition/', data=data)
+        self.assertEqual(r.status_code, 200)
