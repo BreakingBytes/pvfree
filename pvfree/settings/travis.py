37492a2ed62585329c036a94a44276b7676e8f96
Mark Mikofski <mark.mikofski@dnv.com>
2025-03-22 03:30:32 -0700

    secrets cannot be used in pull requests from forked repos

--- pvfree/settings/travis.py
+++ pvfree/settings/travis.py
@@ -7,13 +7,13 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver', '0.0.0.0']
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-PSQL_PSWD = get_secret('PSQL_PSWD', '.postgre')
+# PSQL_PSWD = get_secret('PSQL_PSWD', '.postgre')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'travis_ci_test',
         'USER': 'postgres',
-        'PASSWORD': PSQL_PSWD,
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
