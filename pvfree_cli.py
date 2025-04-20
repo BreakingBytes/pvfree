c9ae58c1bf7191239c286d3b7ca32bee23091a8a
Mark Mikofski <mark.mikofski@dnv.com>
2025-04-20 04:47:57 -0700

    finish pvfree_cli
    - use tqdm for progress
    - add row name to queue
    - add sleep constants, sleep 10ms between records
    - count records wi ncount, return together with failures
    - remove debug logging
    - remove blank lines
    - add error message to exceptions
    - write logs to files
    - show count of created, total count, and failure count

--- pvfree_cli.py
+++ pvfree_cli.py
@@ -7,6 +7,8 @@ import logging
 import urllib
 from datetime import datetime
 import time
+import json
+from tqdm import tqdm
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -15,21 +17,24 @@ LOGGER.setLevel(logging.DEBUG)
 # Create a queue to store the status codes and reasons
 STATUS_QUEUE = queue.Queue()
 SESSION = requests.Session()
+SLEEP = 0.01
 
 
 def push_record_to_api(row, api_url, headers, status_queue, session):
     response = session.post(api_url, json=row, headers=headers)
-    status_queue.put((response.status_code, response.reason))
+    status_queue.put((row['Name'], response.status_code, response.reason))
 
 
-def push_records_to_api(csvfile, api_url, model, user, headers, status_queue, session):
+def push_records_to_api(csvfile, api_url, model, user, headers, status_queue,
+                        session, sleep=SLEEP):
     failures = []
     reader = csv.DictReader(csvfile)
     # skip units header and the sandia header
     next(reader)
     next(reader)
     threads = []
-    for row in reader:
+    ncount = 0
+    for row in tqdm(reader):
         if model == 'cecmodule':
             row = cecmodule_handler(row)
         if 'error' in row:
@@ -42,21 +47,19 @@ def push_records_to_api(csvfile, api_url, model, user, headers, status_queue, se
             args=(row, api_url, headers, status_queue, session))
         threads.append(thread)
         thread.start()
+        time.sleep(sleep)
+        ncount += 1
     
     for thread in threads:
-        time.sleep(0.01)
         thread.join()
 
-    return failures
+    return failures, ncount
 
 
 def cecmodule_handler(kwargs):
-
     # handle BIPV
     bipv = kwargs['BIPV'] == 'Y'
     kwargs['BIPV'] = bipv
-    LOGGER.debug('BIPV = %r', bipv)
-
     # handle date
     timestamp = kwargs['Date']
     try:
@@ -65,15 +68,13 @@ def cecmodule_handler(kwargs):
         LOGGER.exception(exc)
         return {'error': 'date error', 'exc_info': exc, 'data': kwargs}
     kwargs['Date'] = datetime.strftime(timestamp, '%Y-%m-%d')
-    LOGGER.debug('Date = %s', timestamp.isoformat())
-
     # handle Length and Width
     if not kwargs['Length']:
         kwargs['Length'] = None
     if not kwargs['Width']:
         kwargs['Width'] = None
-
     return kwargs
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 6:
@@ -86,53 +87,55 @@ if __name__ == "__main__":
     api_base = sys.argv[3]  # "http://127.0.0.1:8000"
     model = sys.argv[4]  # cecmodule --> /api/v1/cecmodule/
     csv_file_path = sys.argv[5]  # "deploy/libraries/CEC Modules.csv"
-
     try:
         schema = requests.get(urllib.parse.urljoin(api_base, '/api/v1'))
         schema = schema.json()
     except Exception as exc_info:
-        LOGGER.error('api_base not found on server or response not JSON')
-        LOGGER.exception(exc_info=exc_info)
+        msg = 'api_base not found on server or response not JSON'
+        LOGGER.exception(msg, exc_info=exc_info)
         sys.exit(2)
-
     try:
         model_schema = schema[model]
     except KeyError as exc_info:
-        LOGGER.error('model must be in [cecmodule, pvinverter, pvmodule]')
-        LOGGER.exception(exc_info=exc_info)
+        msg = 'model must be in [cecmodule, pvinverter, pvmodule]'
+        LOGGER.exception(msg, exc_info=exc_info)
         sys.exit(3)
     api_url = urllib.parse.urljoin(api_base, model_schema['list_endpoint'])
-
     try:
         users = requests.get(
             urllib.parse.urljoin(api_base, '/api/v1/user'),
             params={'username': username})
         users = users.json()
-    except Exception as exc_info:
-        LOGGER.error("username was not found on the server or response not JSON")
-        LOGGER.exception(exc_info=exc_info)
-        sys.exit(4)
-    else:
         user = users['objects'][-1]['resource_uri']
+    except Exception as exc_info:
+        msg = "username was not found on the server or response not JSON"
+        LOGGER.exception(msg, exc_info=exc_info)
+        sys.exit(4)
     headers = {
         "Authorization": f"ApiKey {username}:{api_key}",
         "Content-Type": "application/json"
     }
-
     try:
         with open(csv_file_path, mode='r', encoding='utf-8') as csvfile:
-            failures = push_records_to_api(
+            failures, ncount = push_records_to_api(
                 csvfile, api_url, model, user, headers, STATUS_QUEUE, SESSION)
     except Exception as exc_info:
-        LOGGER.error("maybe the file wasn't found or something else bad?")
-        LOGGER.exception(exc_info=exc_info)
+        msg = "maybe the file wasn't found or something else bad?"
+        LOGGER.exception(msg, exc_info=exc_info)
         sys.exit(5)
     else:
-        LOGGER.info(failures)
-
+        with open('pvfree_cli_failures.json', 'w', encoding='utf-8') as f:
+            json.dump(failures, f)
+        LOGGER.info(f'count of failures {len(failures)}')
+        LOGGER.info(f'total count of records {ncount}')
     # Process the queue after all threads have finished
-    while not STATUS_QUEUE.empty():
-        status_code, reason = STATUS_QUEUE.get()
-        LOGGER.info(f"Status code: {status_code}, Reason: {reason}")
+    created = 0
+    with open('pvfree_cli.log', 'w', encoding='utf-8') as f:
+        while not STATUS_QUEUE.empty():
+            name, status_code, reason = STATUS_QUEUE.get()
+            if status_code == 201: created += 1
+            f.write(
+                f"Name: {name}, Status code: {status_code}, Reason: {reason}\n")
+    LOGGER.info(f"Created count: {created}")
     SESSION.close()
     sys.exit(0)
